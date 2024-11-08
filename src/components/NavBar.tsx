8795056c9a95bdcc2b5d84cbd0e762789e0e9cfd
Maros Masurik <masurik1@gmail.com>
2024-11-08 13:31:43 +0100

    fixed menu

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,9 @@
-'use client';
+'use client'
 
 import * as React from 'react';
 import { useSession } from 'next-auth/react';
-import { Drawer, List, ListItem, ListItemIcon, ListItemText, IconButton, Divider } from '@mui/material';
+import { Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, IconButton, Divider } from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
-import PersonIcon from '@mui/icons-material/Person';
-import PostAddIcon from '@mui/icons-material/PostAdd';
 import LoginIcon from '@mui/icons-material/Login';
 import LogoutIcon from '@mui/icons-material/Logout';
 import MenuIcon from '@mui/icons-material/Menu';
@@ -15,8 +13,8 @@ export default function NavBar() {
   const { data: session } = useSession();
   const [drawerOpen, setDrawerOpen] = React.useState(false);
 
-  const toggleDrawer = (open) => (event) => {
-    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+  const toggleDrawer = (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+    if (event.type === 'keydown' && ((event as React.KeyboardEvent).key === 'Tab' || (event as React.KeyboardEvent).key === 'Shift')) {
       return;
     }
     setDrawerOpen(open);
@@ -31,7 +29,6 @@ export default function NavBar() {
 
   return (
     <>
-      {/* IconButton to toggle Drawer */}
       <IconButton
         edge="start"
         color="inherit"
@@ -42,14 +39,15 @@ export default function NavBar() {
         <MenuIcon />
       </IconButton>
 
-      {/* Drawer Component */}
       <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer(false)}>
         <List sx={{ width: 250 }}>
           {navigationItems.map((item, index) => (
             <Link href={item.href} key={index} passHref>
-              <ListItem button component="a" onClick={toggleDrawer(false)}>
-                <ListItemIcon>{item.icon}</ListItemIcon>
-                <ListItemText primary={item.label} />
+              <ListItem disablePadding>
+                <ListItemButton component="a" onClick={toggleDrawer(false)}>
+                  <ListItemIcon>{item.icon}</ListItemIcon>
+                  <ListItemText primary={item.label} />
+                </ListItemButton>
               </ListItem>
             </Link>
           ))}
